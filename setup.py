f5b3a6e1f1c3448a34dd45d4a3e955d420bfa82c
runzhouye <84249081+runzhouye@users.noreply.github.com>
2021-05-15 20:13:15 -0400

    Delete setup.py

--- setup.py
+++
@@ -1,7 +0,0 @@
-from cx_Freeze import setup, Executable
-
-setup(name = "DIT" ,
-      version = "1.0.0" ,
-      description = "DESCRIPTION" ,
-      executables = [Executable("DeepImageTranslator.py")]
-)
